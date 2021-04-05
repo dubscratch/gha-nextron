d9b8ef7cfec3b4898fc209b27e40daba8c9e0f8f
Duncan Ward <duncan.ward@uberflip.com>
2021-04-05 14:58:07 -0400

    sign app

---
+++ build/notarize.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+  if (process.env.APPLEID === undefined) {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    appBundleId: 'to.dub.ghanextron',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
